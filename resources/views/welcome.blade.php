3b5fd12c5439eb5111a1b77442fffb98406b6074
IDLKZ <idlkz2019@gmail.com>
2021-01-05 15:53:35 +0600

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="images/front/favicon.ico" type="image/x-icon">
-    <title>Welcome | Aquaponic House</title>
+    <title>Zhasyl El</title>
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="/css/front.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">     <style>
+{{--    <link rel="preconnect" href="https://fonts.gstatic.com">--}}
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <style>
         body {
             font-family: 'Roboto', sans-serif!important;
         }
